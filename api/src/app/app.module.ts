34a57b2b9a65154dd475927fd0cc43d9fc9d1812
Thomas Markiewicz <thomas.markiewicz@protonmail.com>
2025-12-10 15:34:22 -0600

    feat: docker compose for postgresql, nestjs graphql module, scripts

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'apps/api/src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+    }),
+  ],
 })
 export class AppModule {}
